493ac58cddf2b4a495bc6006c2188ea2018c56ac
misha1350 <mishamiha54@gmail.com>
2025-02-16 22:39:48 +0300

    ok

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  base: '/image-editor-web-app/',  // Добавляем базовый путь, соответствующий названию репозитория
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
